219f4ee70dd469af2e219f9309fb071c0322e8d3
Marco Maggi <marco.maggi-ipsu@poste.it>
2014-06-07 10:48:52 +0200

    added internals header file

---
+++ src/cctemplate-internals.h
@@ -0,0 +1,70 @@
+/*
+  Part of: CCTemplate
+  Contents: private header file
+  Date: Thu Mar  1, 2012
+
+  Abstract
+
+	This  header  file is  for  internal  definitions.  It  must  be
+	included by all the source files in this package.
+
+  Copyright (C) 2014 Marco Maggi <marco.maggi-ipsu@poste.it>
+
+  This program is  free software: you can redistribute  it and/or modify
+  it under the  terms of the GNU General Public  License as published by
+  the Free Software Foundation, either  version 3 of the License, or (at
+  your option) any later version.
+
+  This program  is distributed in the  hope that it will  be useful, but
+  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+  General Public License for more details.
+
+  You  should have received  a copy  of the  GNU General  Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCTEMPLATE_INTERNALS_H
+#define CCTEMPLATE_INTERNALS_H 1
+
+
+/** --------------------------------------------------------------------
+ ** Headers.
+ ** ----------------------------------------------------------------- */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+#include "cctemplate.h"
+
+
+/** --------------------------------------------------------------------
+ ** Preprocessor macros.
+ ** ----------------------------------------------------------------- */
+
+
+
+/** --------------------------------------------------------------------
+ ** Constants.
+ ** ----------------------------------------------------------------- */
+
+
+
+
+/** --------------------------------------------------------------------
+ ** Function prototypes .
+ ** ----------------------------------------------------------------- */
+
+
+
+/** --------------------------------------------------------------------
+ ** Done.
+ ** ----------------------------------------------------------------- */
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* CCTEMPLATE_INTERNALS_H */
+
+/* end of file */
--- src/cctemplate-version.c
+++ src/cctemplate-version.c
@@ -7,7 +7,7 @@
 
 
 
-  Copyright (C) 2012 Marco Maggi <marco.maggi-ipsu@poste.it>
+  Copyright (C) 2012, 2014 Marco Maggi <marco.maggi-ipsu@poste.it>
 
   This program is  free software: you can redistribute  it and/or modify
   it under the  terms of the GNU General Public  License as published by
@@ -28,10 +28,7 @@
  ** Headers.
  ** ----------------------------------------------------------------- */
 
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
-#include "cctemplate.h"
+#include "cctemplate-internals.h"
 
 
 const char *
