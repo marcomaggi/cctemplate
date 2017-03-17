12d868214f170b84e4adb4b38d97231ecabc8729
Marco Maggi <marco.maggi-ipsu@poste.it>
2017-03-17 16:03:27 +0100

    code review

--- src/cctemplate.h
+++ src/cctemplate.h
@@ -7,7 +7,7 @@
 
 
 
-  Copyright (C) 2012 Marco Maggi <marco.maggi-ipsu@poste.it>
+  Copyright (C) 2012, 2017 Marco Maggi <marco.maggi-ipsu@poste.it>
 
   This program is  free software: you can redistribute  it and/or modify
   it under the  terms of the GNU General Public  License as published by
@@ -28,7 +28,7 @@
 
 
 /** --------------------------------------------------------------------
- ** Headers.
+ ** Preliminary definitions.
  ** ----------------------------------------------------------------- */
 
 #ifdef __cplusplus
@@ -78,6 +78,13 @@ extern "C" {
 #    define cctemplate_private_decl	extern
 #  endif
 #endif
+
+
+/** --------------------------------------------------------------------
+ ** Headers.
+ ** ----------------------------------------------------------------- */
+
+
 
 
 /** --------------------------------------------------------------------
