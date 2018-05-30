61442faf29fb2912936ab047256b4425cb32728d
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-05-30 07:26:05 +0200

    updated to latest revision of dependencies

--- meta/travis-ci/install-ccmemory.sh
+++ meta/travis-ci/install-ccmemory.sh
@@ -7,7 +7,7 @@
 # script is run from the top directory of the build tree.
 
 PROGNAME=install-ccmemory.sh
-VERSION=0.1.0-devel.3
+VERSION=0.2.0-devel.0
 STEM="ccmemory-${VERSION}"
 ARCHIVE="${STEM}.tar.gz"
 SOURCE_URI="https://github.com/marcomaggi/ccmemory/archive/v${VERSION}.tar.gz"
