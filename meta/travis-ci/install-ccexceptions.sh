214515b8879acebaf116f6ad2e34103bd0e684e7
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-02-22 06:24:29 +0100

    bumped version of ccexceptions for travis builds

--- meta/travis-ci/install-ccexceptions.sh
+++ meta/travis-ci/install-ccexceptions.sh
@@ -7,7 +7,7 @@
 # script is run from the top directory of the build tree.
 
 PROGNAME="${0##*/}"
-VERSION=0.8.0-devel.9
+VERSION=0.8.0-devel.10
 STEM="ccexceptions-${VERSION}"
 ARCHIVE="${STEM}.tar.gz"
 SOURCE_URI="https://github.com/marcomaggi/ccexceptions/archive/v${VERSION}.tar.gz"
