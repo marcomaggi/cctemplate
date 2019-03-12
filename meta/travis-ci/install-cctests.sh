976e0089d1a3a06a49e26b1dba62cc6ee0effcfe
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-03-12 08:31:32 +0100

    update required version number of CCTests when building on travis

--- meta/travis-ci/install-cctests.sh
+++ meta/travis-ci/install-cctests.sh
@@ -7,7 +7,7 @@
 # script is run from the top directory of the build tree.
 
 PROGNAME="${0##*/}"
-VERSION=0.3.0-devel.7
+VERSION=0.4.0-devel.1
 STEM="cctests-${VERSION}"
 ARCHIVE="${STEM}.tar.gz"
 SOURCE_URI="https://github.com/marcomaggi/cctests/archive/v${VERSION}.tar.gz"
